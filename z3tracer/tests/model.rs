306a9209fb0ee1ee2db8aeea15d02fddc6645280
Mathieu Baudet <mathieubaudet@fb.com>
2021-03-14 19:39:10 -0700

    [z3tracer] update tests + add test file

--- z3tracer/tests/model.rs
+++ z3tracer/tests/model.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Facebook, Inc. and its affiliates
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use std::collections::BTreeMap;
+use std::collections::{BinaryHeap, BTreeMap};
 use std::str::FromStr;
 
 use z3tracer::syntax::Ident;
@@ -16,30 +16,68 @@ fn process_file(path: &str) -> std::io::Result<Model> {
     Ok(model)
 }
 
+// TODO(remove after Rust issue 59278 is closed)
+struct IntoIterSorted<T> {
+    inner: BinaryHeap<T>,
+}
+
+impl<T> From<BinaryHeap<T>> for IntoIterSorted<T> {
+    fn from(inner: BinaryHeap<T>) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T: Ord> Iterator for IntoIterSorted<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        self.inner.pop()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let exact = self.inner.len();
+        (exact, Some(exact))
+    }
+}
+
 #[test]
 fn test_file1() -> std::io::Result<()> {
     let model = process_file("tests/data/file1.log")?;
-    assert_eq!(model.terms().len(), 273318);
+    assert_eq!(model.terms().len(), 273321);
     assert_eq!(
         model
             .id_to_sexp(&BTreeMap::new(), &Ident::from_str("#23").unwrap())
             .unwrap(),
         "(PROOF true-axiom true)"
     );
-    assert_eq!(model.instantiations().len(), 3023);
-    let mut top_instantiated = model.most_instantiated_terms();
-    assert_eq!(top_instantiated.len(), 46);
+    assert_eq!(model.instantiations().len(), 21503);
+    let top_instantiated = model.most_instantiated_terms();
+    assert_eq!(top_instantiated.len(), 70);
+    let mut top_instantiated : IntoIterSorted<_> = top_instantiated.into();
     assert_eq!(
-        top_instantiated.pop().unwrap(),
+        top_instantiated.next().unwrap(),
+        (7903, Ident::from_str("basic#").unwrap())
+    );
+    let mut top_instantiated = top_instantiated.filter(|(_, id)| id.namespace.is_none());
+    assert_eq!(
+        top_instantiated.next().unwrap(),
         (359, Ident::from_str("#4429!9").unwrap())
     );
     assert_eq!(
-        top_instantiated.pop().unwrap(),
+        top_instantiated.next().unwrap(),
         (359, Ident::from_str("#4328").unwrap())
     );
     assert_eq!(
-        top_instantiated.pop().unwrap(),
+        top_instantiated.next().unwrap(),
         (259, Ident::from_str("#23065!1").unwrap())
     );
+    Ok(())
+}
+
+#[test]
+fn test_file2() -> std::io::Result<()> {
+    let model = process_file("tests/data/file2.log")?;
+    assert_eq!(model.terms().len(), 150031);
+    assert_eq!(model.instantiations().len(), 10242);
     Ok(())
 }
